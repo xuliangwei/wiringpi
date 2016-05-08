8d0fe3c0604e225d9273858a7606ba3c5b7c53b8
xuliangwei <liangwei.xu@gmail.com>
2016-05-08 23:51:30 +0800

    ds18b20: 解决上电第一次读到无效的温度(85.000度)的问题.

--- ds18b20/ds18b20.cpp
+++ ds18b20/ds18b20.cpp
@@ -9,7 +9,8 @@
 
 #define DEBUG 0
 
-int main(void) {
+int get_temperature(double *temperature)
+{
 	const char	*c_device = "/sys/bus/w1/devices/28-800000264ccc/w1_slave";
 	int fd = open(c_device, O_RDONLY);
 	if (fd < 0) {
@@ -33,10 +34,27 @@ int main(void) {
 	if (strstr(buf, "crc=") && strstr(buf, "YES")) {
 		const char *t = strstr(buf, "t=");
 		if (t) {
-			double temperature = (double)atol(t+2) / 1000;
-			printf("Temperature: %.3f\n", temperature);
+			if (temperature)
+			*temperature = (double)atol(t+2) / 1000;
 		}
 	}
 	close(fd);
+	return 0;
+}
+
+int main(void) {
+	const double c_delta = 1;
+	double last_temperature;
+	double temperature = -300;
+
+	do {
+		last_temperature = temperature;
+		int status = get_temperature(&temperature);
+		if ( status < 0) {
+			printf("Read temerature failed. error no: %d\n", status);
+			return status;
+		}
+	} while (abs(temperature - last_temperature) > c_delta);
+	printf("Temperature: %.3f\n", temperature);
 	return 0;
 }
